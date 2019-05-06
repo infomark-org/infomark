d725269a8a7498aae1dbb07786bed4c88b002661
PatWie <mail@patwie.com>
2019-05-06 14:08:46 +0200

    tutors will get the email as well when contacting their group

--- api/app/group.go
+++ api/app/group.go
@@ -488,6 +488,14 @@ func (rs *GroupResource) SendEmailHandler(w http.ResponseWriter, r *http.Request
 		return
 	}
 
+	msgOwn := email.NewEmailFromUser(
+		accessUser.Email,
+		data.Subject,
+		data.Body,
+		accessUser,
+	)
+	email.OutgoingEmailsChannel <- msg
+
 	for _, recipient := range recipients {
 		msg := email.NewEmailFromUser(
 			recipient.Email,
