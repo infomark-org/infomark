811629a0ba8e1c2cd623929b59c7b1a9be46ce7b
Raphael <keyraphi@gmail.com>
2021-08-23 15:40:23 +0200

    The external URL did not work for file access.

--- api/worker/submission_handler.go
+++ api/worker/submission_handler.go
@@ -274,12 +274,14 @@ func (h *RealSubmissionHandler) Handle(body []byte) error {
 	workerResp.EnqueuedAt = msg.EnqueuedAt
 	workerResp.StartedAt = time.Now()
 
+	fmt.Println("Running docker!")
 	stdout, exit, err = ds.Run(
 		msg.DockerImage,
 		submissionPath,
 		frameworkPath,
 		int64(configuration.Configuration.Worker.Docker.MaxMemory),
 	)
+	fmt.Println("stdout", stdout)
 	if err != nil {
 		DefaultLogger.WithFields(logrus.Fields{
 			"submissionID": msg.SubmissionID,
